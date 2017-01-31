20e387d70332ab0db0297671827dde392cce68f0
AvaHenriksen <ava.henriksen@gmail.com>
2017-01-30 18:43:45 -0600

    Bad version

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,14 +1,14 @@
 class GuessesController < ApplicationController
 
-  def check_obedience
-  if params[:first_number] < params[:second_number] < params[:third_number]
-    @obedience_answer = "Obeys the rule"
-  elsif params[:first_number] > params[:second_number] > params[:third_number]
+  def attempt
+    if (params[:first_number] < params[:second_number]) && (params[:second_number] < params[:third_number])
+      @obedience_answer = "Obeys the rule"
+    elsif (params[:first_number] > params[:second_number]) || (params[:first_number] = params[:second_number]) || (params[:second_number] > params[:third_number]) || (params[:second_number] > params[:third_number]) || (params[:first_number] > params[:third_number]) || (params[:first_number] = params[:third_number])
+      @obedience_answer = "Does not obey the rule"
 
-    @obedience_answer = "Does not obey the rule"
-
+    end
+    render ("attempt.html.erb")
   end
-  end
 
-  render ("attempt.html.erb")
+
 end
